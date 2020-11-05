77df82ddddf526c735638bc29e4401a92b87814f
“Shwetha <shwethabalaji87@gmail.com>
2020-11-05 20:29:42 +0530

    Password Validation

--- UserVerification.sh
+++ UserVerification.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-read -p "Enter your Mobile Number : " num
+echo "Your Password must :"
+echo "1.Have atleast 1 UpperCase Character"
+echo "2.Have atleast 1 Numeric Value"
+echo "3.Has exactly 1 Special Character"
+echo " "
+read -p "Enter a Valid Password : " pass
 
-pat="^[0-9]{2}[+][ ]{1}[0-9]{10}$"
-if [[ $num =~ $pat ]]
+pat="^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z)(?=.*!@#$%^&*).{8,}$"
+if [[ $pass =~ $pat ]]
 then
-        echo "Valid Mobile Number"
+        echo "Valid Password"
 else
-        echo "Invalid Mobile Number"
+        echo "Invalid Password"
 fi
+
+
+
+
+
+
+
+
